e35f49779bcbcf00feadd8ac8aab637b13afe98a
chirantan-basu01 <basuchirantan001@gmail.com>
2021-05-17 16:39:23 +0530

    initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                {/* Nav section */}
+                <Nav />
+                {/* Landing Page */}
+
+                {/* Challenge section */}
+
+                {/* Footer */}
+            </div>
+        );
+    }
+}
+export default App;
