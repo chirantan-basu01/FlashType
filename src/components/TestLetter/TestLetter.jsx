941153eee5993c73022f02334e3f1867fb7b163b
chirantan-basu01 <basuchirantan001@gmail.com>
2021-05-22 16:28:51 +0530

    enable: retry button

--- src/components/TestLetter/TestLetter.jsx
+++ src/components/TestLetter/TestLetter.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './TestLetter.css';
 
 const TestLetter = ({ individualLetterInfo }) => {
-    const { status } = individualLetterInfo;
-
-    const statusClass = {
+    const statusClassName = {
         correct: 'test-letter-correct',
         incorrect: 'test-letter-incorrect',
-        notAttempted: 'test-letter-notAttempted',
-    }[status];
+        notAttempted: 'test-letter-not-attempted',
+    }[individualLetterInfo.status];
 
     return (
-        <span className={`test-letter ${statusClass}`}>
+        <span className={`test-letter ${statusClassName}`}>
             {individualLetterInfo.testLetter}
         </span>
     );
--- src/components/TryAgain/TryAgain.jsx
+++ src/components/TryAgain/TryAgain.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TryAgain.css';
 
-const TryAgain = ({ words, characters, wpm }) => {
+const TryAgain = ({ words, characters, wpm, startAgain }) => {
     return (
         <div className="try-again-container">
             <h1>Test Results</h1>
@@ -17,7 +17,12 @@ const TryAgain = ({ words, characters, wpm }) => {
                 </p>
             </div>
             <div>
-                <button className="end-buttons start-again-btn">Re-try</button>
+                <button
+                    onClick={() => startAgain()}
+                    className="end-buttons start-again-btn"
+                >
+                    Re-try
+                </button>
                 <button
                     onClick={() => {
                         window.open(
